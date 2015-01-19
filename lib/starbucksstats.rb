6d04a0ef45ea4b4d5cf65e2b1ce3e5df16bb9524
RSid <sm1th303@gmail.com>
2015-01-19 00:02:41 -0500

    passing tests for getting links to all beverage types and list of beverage types

--- lib/starbucksstats.rb
+++ lib/starbucksstats.rb
@@ -1,5 +1,38 @@
 require "starbucksstats/version"
+require 'nokogiri'
+require 'rest-client'
+require 'pry'
 
 module Starbucksstats
-  # Your code goes here...
+  ROOT_URL = "http://www.starbucks.ie/menu/"
+
+
+  def self.get_beverage_types_links
+    beverage_info = self.get_beverage_info
+
+    beverage_links = []
+    beverage_info.each do |bvg|
+      beverage_links << bvg["href"]
+    end
+
+    beverage_links
+  end
+
+  def self.get_beverage_types
+    beverage_info = self.get_beverage_info
+
+    beverages = []
+    beverage_info.each do |bvg|
+      beverages << bvg.text
+    end
+
+    beverages
+  end
+
+  private
+
+  def self.get_beverage_info
+    @beverage_page = Nokogiri::HTML(RestClient.get(ROOT_URL + 'beverage-list'))
+    beverage_info = @beverage_page.css('ol li dl dt a')
+  end
 end
--- spec/starbucksstats_gets_beverage_list_spec.rb
+++ spec/starbucksstats_gets_beverage_list_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper.rb'
 
-describe "#starbucksstats get beverage types" do
-  it 'gets a list of all available beverage types' do
+describe "#starbucksstats get beverage type links" do
+  it 'gets a list of links to all available beverage types' do
+    ROOT_RETURNED_URL = '/menu/beverage-list/'
+
     actual_beverages = ['bottled-drinks', 'brewed-tea', 'chocolate-beverages',
     'espresso-beverages', 'brewed-coffee', 'frappucino-blended-coffee']
 
-    beverages = Starbucksstats.get_beverage_types
+    beverages = Starbucksstats.get_beverage_types_links
 
     expect(beverages.length).to be(actual_beverages.length)
-    expect(beverages).include?(actual_beverages[0]).to eql(true)
+    expect(beverages.include?(ROOT_RETURNED_URL + actual_beverages[0])).
+    to eql(true)
   end
 end
+
+describe "#starbucksstats get beverage types" do
+  it 'gets a list of all available beverage types' do
+
+    actual_beverages = ['Bottled Drinks', 'Brewed Tea', 'Chocolate Beverages',
+      'Espresso Beverages', 'Filter Coffee', 'Frappucino Blended Beverages']
+
+      beverages = Starbucksstats.get_beverage_types
+
+      expect(beverages.length).to be(actual_beverages.length)
+      expect(beverages.include?(actual_beverages[0])).to eql(true)
+    end
+  end
