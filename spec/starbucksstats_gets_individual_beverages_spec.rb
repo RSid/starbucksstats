fb49b3552bcdc642161b68ee77392e325f647ad9
RSid <sm1th303@gmail.com>
2015-01-19 11:57:20 -0500

    test for getting list of links to all individual drinks

---
+++ spec/starbucksstats_gets_individual_beverages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper.rb'
+
+describe "#starbucksstats gets links for all beverages" do
+  it 'gets a list of links for each individual beverage' do
+    ROOT_RETURNED_URL = '/menu/beverage-list/'
+
+    sample_list_of_actual_beverage_links = ['espresso-beverages/espresso',
+    'frappuccino-blended-coffee/caramel-frappuccino-blended-beverage',
+    'brewed-tea/china-green-tips-brewed-tea',
+    'chocolate-beverages/vanilla-spice-hot-chocolate',
+    'bottled-drinks/starbucks-bottled-coffee-frappuccino-mocha-chocolate-drink']
+
+    beverages = Starbucksstats.get_beverage_types_links
+
+    expect(beverages.length).to be > 0
+    expect(beverages.include?(ROOT_RETURNED_URL +
+    sample_list_of_actual_beverage_links[0])).to eql(true)
+    expect(beverages.include?(ROOT_RETURNED_URL +
+    sample_list_of_actual_beverage_links[-1])).to eql(true)
+  end
+end
