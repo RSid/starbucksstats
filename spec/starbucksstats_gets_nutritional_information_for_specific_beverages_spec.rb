22df1f7f0c0bd0ac635af25193f7252c7aceee0b
RSid <sm1th303@gmail.com>
2015-01-19 15:51:06 -0500

    added prelim test for getting nutritional data for a specific drink out of pdf

--- spec/starbucksstats_gets_nutritional_information_for_specific_beverages_spec.rb
+++ spec/starbucksstats_gets_nutritional_information_for_specific_beverages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper.rb'
 describe "#starbucksstats gets nutritional information for individual beverages" do
   it "gets a pdf with an individual beverages' nutritional data" do
 
-    pdf_string = Starbucksstats.get_all_nutritional_info('Espresso')
+    pdf_string = Starbucksstats.get_nutritional_info('Espresso')
 
     expect(pdf_string.include?('Venti')).to be(true)
   end
